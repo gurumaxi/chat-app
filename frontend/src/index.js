bed9938ee870f90ea251c70e64b6af84235875e7
gurumaxi <maxrialto98@gmail.com>
2022-04-11 16:02:07 +0200

    replaced css with scss

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./global.css";
+import "./global.scss";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
